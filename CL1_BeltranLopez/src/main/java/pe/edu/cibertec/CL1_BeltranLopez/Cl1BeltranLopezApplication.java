373ec0e9d93c63e062f8a5d766f33a56e0c51db2
GianBeltran <Gbeltranl0506@gmail.com>
2023-05-07 10:10:13 -0500

    Primer guardado(Proyecto creado sin ningun proceso)

---
+++ CL1_BeltranLopez/src/main/java/pe/edu/cibertec/CL1_BeltranLopez/Cl1BeltranLopezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_BeltranLopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1BeltranLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1BeltranLopezApplication.class, args);
+	}
+
+}
---
+++ CL1_BeltranLopez/src/test/java/pe/edu/cibertec/CL1_BeltranLopez/Cl1BeltranLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_BeltranLopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1BeltranLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
